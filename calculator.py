a6d4c1e8fcb8ad1cae9bae820b36849a1022a87c
Jade1724 <haruka.i1724@gmail.com>
2022-05-23 16:14:06 +1200

    Tested importing specific function from calulator module.

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def addNumbers(a, b):
+    print("Sum is ", a + b)
+
+def subtractNumbers(a, b):
+    print("Difference is ", a - b)
+
+def multiplyNumbers(a, b):
+    print("Product is ", a * b)
+
+def divideNumbers(a, b):
+    if not b == 0:
+        print("Division is", a / b)
+    else:
+        print("Cannot devide by zero")
+
+def modulosNumbers(a, b):
+    print("Remainder is ", a % b)
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from helper import *
+from calculator import addNumbers, divideNumbers
 
 say_hello()
+addNumbers(3, 4)
+divideNumbers(7, 0)
+divideNumbers(1024, 16)
